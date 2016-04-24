4c860db82d8cbbbdb16cf42f34260f7541b6b550
retrorocket <unrealenemy@gmail.com>
2016-04-24 13:06:04 +0900

    add stat/ink mode

--- API/script.pl
+++ API/script.pl
@@ -27,6 +27,7 @@ app->hook('before_dispatch' => sub {
     $self->res->headers->header('Access-Control-Allow-Origin' => '*');
 });
 
+### 定数とか ###
 #for debug
 #my $log = app->log;
 
@@ -42,20 +43,83 @@ my $ERROR_404 = "Not found.";
 # collection map
 my $collection_map = {gachi => $GACHI, regular => $REGULAR, fes => $FES};
 
+# stat.ink stage map
+my $stat_ink_map = {
+    "アンチョビットゲームズ" => "anchovy",
+    "アロワナモール" => "arowana",
+    "Ｂバスパーク" => "bbass",
+    "デカライン高架下" => "dekaline",
+    "ハコフグ倉庫" => "hakofugu",
+    "ヒラメが丘団地" => "hirame",
+    "ホッケふ頭" => "hokke",
+    "キンメダイ美術館" => "kinmedai",
+    "マヒマヒリゾート＆スパ" => "mahimahi",
+    "マサバ海峡大橋" => "masaba",
+    "モンガラキャンプ場" => "mongara",
+    "モズク農園" => "mozuku",
+    "ネギトロ炭鉱" => "negitoro",
+    "シオノメ油田" => "shionome",
+    "ショッツル鉱山" => "shottsuru",
+    "タチウオパーキング" => "tachiuo"
+};
+
+# stat.ink rule map
+my $stat_ink_rule_map = {
+    "ガチエリア" => "area",
+    "ガチヤグラ" => "yagura",
+    "ガチホコ" => "hoko"
+};
+
+### サブルーチン ###
 # レスポンスとして返却するJSONのマップ
 sub create_result_map(){
     my $mode = shift;
     my $entry = shift;
+    my $stat_ink_mode = shift;
+    
     my $start = $entry->{start}->set_time_zone($TZ);
     my $end = $entry->{end}->set_time_zone($TZ);
     my $result_map = {start=>$start->datetime, end=>$end->datetime, maps=>$entry->{maps}};
+    if($stat_ink_mode){
+        my $origin_maps = $entry->{maps};
+        $result_map->{maps} = &convert_stat_ink_map($origin_maps);
+    }
     if($mode eq $GACHI) {
         $result_map->{rule} = $entry->{rule};
+        if($stat_ink_mode){
+            my $origin_rule = $entry->{rule};
+            $result_map->{rule} = &convert_stat_ink_rule($origin_rule);
+        }
     }
     if($mode eq $FES) {
         $result_map->{team} = $entry->{team};
     }
     return $result_map;
+}
+
+# stat.ink用のstage mapに変換する
+sub convert_stat_ink_map(){
+    my $origin_maps = shift;
+    my @results;
+    for my $elem (@$origin_maps){
+        my $stat_elem = $stat_ink_map->{$elem};
+        unless($stat_elem){
+            die("unknown map"); #即死
+        }
+        my $result = {origin_name => $elem, stat_ink_name => $stat_elem};
+        push(@results, $result);
+    }
+    return \@results;
+}
+# stat.ink用のrule mapに変換する
+sub convert_stat_ink_rule(){
+    my $origin_rule = shift;
+    my $stat_elem = $stat_ink_rule_map->{$origin_rule};
+    unless($stat_elem){
+        die("unknown rule"); #即死
+    }
+    my $result = {origin_name => $origin_rule, stat_ink_name => $stat_elem};
+    return $result;
 }
 
 # in句を使用する
@@ -95,6 +159,7 @@ sub one_get(){
     my $mode = shift;
     my $query = shift;
     my $position = shift;
+    my $stat_ink_mode = shift;
 
     my $collection_name  = $collection_map->{$mode};
     unless($collection_name) {
@@ -132,7 +197,7 @@ sub one_get(){
     unless($entry){
         return \@result;
     }
-    my $result_map = &create_result_map($mode, $entry);
+    my $result_map = &create_result_map($mode, $entry, $stat_ink_mode);
     push(@result, $result_map);
     return \@result;
 }
@@ -142,6 +207,7 @@ sub all_get(){
     my $mode = shift;
     my $query = shift;
     my $position = shift;
+    my $stat_ink_mode = shift;
     
     my $collection_name = $collection_map->{$mode};
     unless($collection_name) {
@@ -179,7 +245,7 @@ sub all_get(){
     my $all = $collection->find($query_map);
     my @result;
     while ( my $entry = $all->next ) {
-        my $result_map = &create_result_map($mode, $entry);
+        my $result_map = &create_result_map($mode, $entry, $stat_ink_mode);
         push(@result, $result_map);
     }
     return \@result;
@@ -196,6 +262,7 @@ sub array_get(){
     return $all->{$mode};
 }
 
+### Routing ###
 get '/' => sub {
     my $self = shift;
     return $self->render(template => 'index', format => 'html');
@@ -237,12 +304,16 @@ get '/:mode/:time' => sub {
     my $self = shift;
     my $mode = $self->param('mode');
     my $time = $self->param('time');
+    my $stat_ink_mode = 0;
+    if($self->param('stat_ink') eq "on"){
+        $stat_ink_mode = 1;
+    }
     my $query = $self->req->params->to_hash;
     my $result;
     if($time eq "now" || $time eq "next" || $time eq "prev" ){
-        $result = &one_get($mode, $query, $time);
+        $result = &one_get($mode, $query, $time, $stat_ink_mode);
     } elsif($time eq "next_all" || $time eq "prev_all") {
-        $result = &all_get($mode, $query, $time);
+        $result = &all_get($mode, $query, $time, $stat_ink_mode);
     } else {
         return $self->render(json =>{'error' => $ERROR_404}, status => '404');
     }
